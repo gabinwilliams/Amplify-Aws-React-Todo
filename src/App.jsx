d2913f2a3e27ec28b79b18f4dfdb7cbae4247254
gabinwilliams <scoutsnknives@gmail.com>
2021-09-08 14:27:56 -0500

    added aws stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {API} from 'aws-amplify';
+import {createTodo} from './graphql/mutations';
+import {withAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react';
 
 function App() {
+
+  const [todo, setTodo] = useState([])
+  const [todoData, setTodoData] = useState({name: '', description: ''})
+  const handleAddTodo = () => {
+    const name = window.prompt('Enter a name');
+    const description = window.prompt('Enter a description');
+
+   
+
+    setTodo([{name, description}, ...todo])
+
+    API.graphql({
+      query: createTodo, 
+      variables: {input: {name, description, tags: []}},
+    }).then((response) => console.log(response))
+  }
+
+//   API.graphql({
+//     query: createTodo, 
+//     variables: {input: {name, description, tags: []}},
+//   }).then((response) => console.log(response))
+// }
+
+  
+
   return (
     <div className="App">
+      <div>
+        <AmplifySignOut/>
+      </div>
       <h1>Todos</h1>
-      <button>Add Task</button>
+      <button onClick={handleAddTodo}>Add Task</button>
       <main>
         <article>
           <h3>Todo Title</h3>
@@ -15,4 +46,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
-
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+// gives frontend access to backend
+Amplify.configure(config)
 ReactDOM.render(
   <React.StrictMode>
     <App />
