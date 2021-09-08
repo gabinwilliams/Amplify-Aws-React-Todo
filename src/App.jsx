7d8f973c963d9d29a4eb7b0258c1a0be14234ef2
gabinwilliams <scoutsnknives@gmail.com>
2021-09-08 10:34:37 -0500

    Install required packages, setup barebones html structure

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Todos</h1>
+      <button>Add Task</button>
+      <main>
+        <article>
+          <h3>Todo Title</h3>
+          <p>Todo Description</p>
+        </article>
+      </main>
+    </div>
+  );
+}
+
+export default App;
