e0f313c6aacbe097192c4da6154b7cee49fe3e18
gabinwilliams <scoutsnknives@gmail.com>
2021-09-08 21:08:32 -0500

    fixed cognito pool issues, current build works with no errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import React, {useState} from 'react';
 import {API} from 'aws-amplify';
 import {createTodo} from './graphql/mutations';
+// import {getTodo} from './graphql/queries';
 import {withAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react';
 
 function App() {
 
   const [todo, setTodo] = useState([])
   const [todoData, setTodoData] = useState({name: '', description: ''})
+
   const handleAddTodo = () => {
     const name = window.prompt('Enter a name');
     const description = window.prompt('Enter a description');
 
    
+
 
     setTodo([{name, description}, ...todo])
 
@@ -19,13 +22,12 @@ function App() {
       query: createTodo, 
       variables: {input: {name, description, tags: []}},
     }).then((response) => console.log(response))
+
+ 
   }
 
-//   API.graphql({
-//     query: createTodo, 
-//     variables: {input: {name, description, tags: []}},
-//   }).then((response) => console.log(response))
-// }
+ 
+
 
   
 
